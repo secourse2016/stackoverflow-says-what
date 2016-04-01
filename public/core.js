37f869a23f04109ab0e69cb9c26585dc00333944
Esraa <esraasalah171995@gmail.com>
2016-04-01 18:35:51 +0200

    angular + intial routing

---
+++ public/core.js
@@ -0,0 +1,13 @@
+App = angular.module('SE-Project',['ui.bootstrap', 'ngRoute']);
+App.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : '/partials/main.html',
+            controller  : 'mainCtrl'
+        })
+        .otherwise({ 
+      redirectTo: '/' 
+    });
+});
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app     = require('./app/app');
 
-app.listen('80', function(){
-  console.log('[OK] => HTTP Server listening on http://localhost:80');
+app.listen('3001', function(){
+  console.log('[OK] => HTTP Server listening on http://localhost:3001');
 });
