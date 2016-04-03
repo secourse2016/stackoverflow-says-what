46a1f8745b8bf4c3579d9ae6f2ef30c0d6b669fc
loay <loayelzobeidy@gmail.com>
2016-04-03 14:56:16 +0200

    outgoingflights page

--- public/controllers/flightCtrl.js
+++ public/controllers/flightCtrl.js
@@ -4,34 +4,68 @@ App.controller('flightCtrl', function($scope,flightSrv,$location) {
 		{"continent": "1",
         "type": "airport",
         "lat": "53.87825",
-        "size": "medium"},
+        "size": "medium" ,
+        "price1": 10000,
+        "price2": 100,
+        "price3": 10},
         {"continent": "2",
         "type": "airport",
         "lat": "53.87825",
-        "size": "medium"},
+        "size": "medium",
+        "price1": 1000,
+        "price2": 200,
+        "price3": 60
+    },
         {"continent": "3",
         "type": "airport",
         "lat": "53.87825",
-        "size": "medium"}];
-        function reservebusiness(flight){
+        "size": "medium",
+         "price1": 3000,
+        "price2": 210,
+        "price3": 101}];
+       $scope.reservebusiness= function (flight){
         	flightSrv.setFlight(flight);
         		flightSrv.setPrice(flight);
         			flightSrv.setClass("business");
           	console.log('hey');
-          	$location.path("../partials/payment.html");
+          	$location.url("/payment.html");
           
         };
-          reserveeconomyA =function(flight){
+          $scope.reserveeconomyA =function(flight){
         	flightSrv.setFlight(flight);
         		flightSrv.setPrice(flight);
         			flightSrv.setClass("economyA");
-           $location.path("../partials/payment.html");
+           $location.url("/payment.html");
         };
-          reserveeconomyB =function(flight){
+          $scope.reserveeconomyB =function(flight){
         	flightSrv.setFlight(flight);
         		flightSrv.setPrice(flight);
         			flightSrv.setClass("economyBz");
-           $location.path("../partials/payment.html");
-        };
+           $location.url("payment.html");
+    };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+
+  $scope.open1 = function() {
+    $scope.popup1.opened = true;
+  };
+
+  $scope.open2 = function() {
+    $scope.popup2.opened = true;
+  };
+
+  $scope.setDate = function(year, month, day) {
+    $scope.dt = new Date(year, month, day);
+  };
+
+  $scope.popup1 = {
+    opened: false
+  };
+
+  $scope.popup2 = {
+    opened: false
+  };
   
+
 });
