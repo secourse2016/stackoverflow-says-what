f103a133102da81a3cf857a2dc7cfa2998f4aa43
RaniaWael <rania.waelsameh.rw@gmail.com>
2016-04-15 00:44:11 +0200

    RoundTrip method + 1st 2 method in app.js + representing the airport name in bookingFlight partial

--- app/app.js
+++ app/app.js
@@ -1,7 +1,117 @@
 var express       = require('express');
 var app           = express();
+var flights       = require('../flights.js');
+var db            = require('../db.js');
 
 app.use(express.static('public'));
 require('./routes')(app);
 
 module.exports = app;
+
+//
+/* SEED DB */
+    app.get('/db/seed', function(req, res) {
+    	flights.seedFlights(function(err, seeded){
+            console.log('seeded the flights');
+            res.send(seeded);
+        });
+    });      
+
+    /* DELETE DB */
+    app.get('/db/delete', function(req, res) {
+    	db.clearDB(function(done){
+            console.log('DB is deleted');
+            res.send(done);
+    
+        });
+    });     
+
+    /* ******************************************** */
+    /*  ROUTES IN WHICH YOUR APPLICATION REQUIRES   */
+    /* ******************************************** */
+
+    /* Middleware */
+   /* app.use(function(req, res, next) {
+    });*/
+
+    /**
+     * ROUND-TRIP SEARCH REST ENDPOINT
+     * @param origin - Flight Origin Location
+     * @param destination - Flight Destination Location
+     * @param departingDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+     * @param returningDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+     * @param class - economy or business only
+     * @returns {Array}
+     */        
+    app.get('/api/flights/search/:origin/:destination/:departingDate/returningDate/:class', function(req, res) {
+        // retrieve params from req.params.{{origin | departingDate | ...}}
+        // return this exact format
+        res = flights.getRoundTripFlightFromDB(cb, ":origin", ":destination", ":departingDate", ":returningDate", ":class");
+/*
+        return 
+        {
+          outgoingFlights: 
+            [{
+                "flightNumber"      : "SE2804",
+                "aircraftType"      : "Boeing",
+                "aircraftModel"     : "747",
+                "departureDateTime" : 1460478300000,
+                "arrivalDateTime"   : 1460478300000,
+                "origin"            : "JFK",
+                "destination"       : "CAI",
+                "cost"              : "750",
+                "currency"          : "USD",
+                "class"             : "economy",
+                "Airline"           : "United"
+            },
+            {
+                        // more flights
+            }],
+          returnFlights:
+            [{
+                "flightNumber"      : "SE2805",
+                "aircraftType"      : "Boeing",
+                "aircraftModel"     : "747",
+                "departureDateTime" : 1460478300000,
+                "arrivalDateTime"   : 1460478300000,
+                "origin"            : "CAI",
+                "destination"       : "JFK",
+                "cost"              : "845",
+                "currency"          : "USD",
+                "class"             : "economy",
+                "Airline"           : "United"
+            }]
+        };*/
+    });  
+
+    /**
+     * ONE-WAY SEARCH REST ENDPOINT 
+     * @param origin - Flight Origin Location
+     * @param DepartingDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+     * @param class - economy or business only
+     * @returns {Array}
+     */    
+    app.get('/api/flights/search/:origin/:departingDate/:class', function(req, res) {
+        // retrieve params from req.params.{{origin | departingDate | ...}}
+        // return this exact format
+        res = flights.getOneWayFlightFromDB(cb, ":origin", ":destination", ":departingDate", ":class");
+       /* return 
+        {
+          outgoingFlights: 
+            [{
+                "flightNumber"      : "SE2804",
+                "aircraftType"      : "Airbus",
+                "aircraftModel"     : "A320",
+                "departureDateTime" : 1460478300000,
+                "arrivalDateTime"   : 1460478300000,
+                "origin"            : "JFK",
+                "destination"       : "CAI",
+                "cost"              : "1567",
+                "currency"          : "USD",
+                "class"             : "economy",
+                "Airline"           : "United"
+            }]
+        };*/
+    });
+
+    /* STRIPE REST ENDPOINT -> NEXT SPRINT */
--- flights.js
+++ flights.js
@@ -1,6 +1,32 @@
-var flights  = require('./flights.json');
+//var flights  = require('./flights.json');
 var airports = require('./airports.json');
 var myDB = require('./db.js');
+var flight = {
+	  "flight_no": "SE28099",
+	  "aircraft_model": "Airbus a318",
+	  "origin": "IAD",
+	  "destination": "JFK",
+	    "capacity": "100",
+	    "date": "2016-04-23T18:25:43.511Z",
+	    "duration": 120,
+	    "available_seats" : {
+	    "seats_a" : 20,
+	    "seats_b" : 32,
+	    "seats_c" : 38 
+	    },
+	    "seatmap":  [{
+	        "seat_no" : "A23", 
+	              "class" : "A", 
+	              "reservation_id" : "VIW23Jfwq8vi3x0ef9",
+	              "cost" : 321,
+	              "window" : true,
+	              "cabin_no": "3"
+	              }],
+	     "price1"  :1000,
+	     "price2"   :100,
+	     "price3"   :80
+
+	};
 
 exports.getAirportsFromJSON = function(){
 	return airport;
@@ -37,3 +63,98 @@ exports.seedFlights = function(cb){
 		}
 	});
 };
+
+//var Database = require("./db.js");
+exports.getOneWayFlightFromDB = function (cb,origin,destination,departingDate,myClass)
+{
+	myDB.db().collection("flights").find({"origin": origin, "destination": destination, "date": departingDate, "class":myClass}).toArray(function(err,flightsArray)
+	{
+    	if (!err)
+    	{
+    		var res = {};
+    		if(myClass == "economy" && flightsArray.available_seats.seats_a > 0){
+    		res.insert({"flightNumber": flightsArray.flight_no, "aircraftType": flightsArray.aircraft_type,
+    					"aircraftModel": flightsArray.aircraft_model, "departureDateTime": flightsArray.date,
+    					"arrivalDateTime": flightsArray.arrival_date, "origin": flightsArray.origin, 
+    					"destination": flightsArray.destination, "cost": flightsArray.price1, "currency": "USD",
+    					"class": "economy", "Airline":"Hawaiian"});
+    	}
+    	else if (myClass == "business" && flightsArray.available_seats.seats_b > 0){
+    		res.insert({"flightNumber": flightsArray.flight_no, "aircraftType": flightsArray.aircraft_type,
+    					"aircraftModel": flightsArray.aircraft_model, "departureDateTime": flightsArray.date,
+    					"arrivalDateTime": flightsArray.arrival_date, "origin": flightsArray.origin, 
+    					"destination": flightsArray.destination, "cost": flightsArray.price2, "currency": "USD",
+    					"class": "business", "Airline":"Hawaiian"});
+    	}
+    		//cb(err,flightsArray);
+    		//console.log(flightsArray);
+    		cb(err,res);
+    		console.log(res);
+    	}
+    	//var myFlight = flightsArray[0];
+    	/*if (myFlight.available_seats.seats_a > 0)
+    		console.log('success');
+    	else
+    		console.log('failure');*/
+    	//cb(err,flightsArray);
+
+    });
+};
+
+exports.getRoundTripFlightFromDB = function(cb, origin, destination, departingDate, returningDate, myClass)
+{
+	return {
+		outgoingFlights: getOneWayFlightFromDB(cb, origin, destination, departingDate, myClass),
+		returningFlights: getOneWayFlightFromDB(cb, destination, origin, returningDate,  myClass)
+	};
+};
+/*var myFlight = {
+	  "flight_no": "SE28099",
+	  "aircraft_model": "Airbus a318",
+	  "origin": "IAD",
+	  "destination": "JFK",
+	    "capacity": "100",
+	    "date": "2016-04-23T18:25:43.511Z",
+	    "duration": 120,
+	    "available_seats" : {
+	    "seats_a" : 20,
+	    "seats_b" : 32,
+	    "seats_c" : 38 
+	    },
+	    "seatmap":  [{
+	        "seat_no" : "A23", 
+	              "class" : "A", 
+	              "reservation_id" : "VIW23Jfwq8vi3x0ef9",
+	              "cost" : 321,
+	              "window" : true,
+	              "cabin_no": "3"
+	              }],
+	     "price1"  :1000,
+	     "price2"   :100,
+	     "price3"   :80
+
+	};
+	var myClass = 'economy';
+	var flightsArray = [];
+	var result = {};
+	result.flightNumber = myFlight.flight_no;
+	//result.aircraftType = ;
+	//result.aircraftModel = ;
+	//result.departureDateTime = ;
+	//result.arrivalDateTime = ;
+	result.origin = myFlight.origin;
+	result.destination = myFlight.destination;
+	result.currency = "USD";
+	//flightsArray[0]=myFlight;
+	if (myClass === 'economy')
+	{
+		if (myFlight.available_seats.seats_a > 0)
+		{
+			result.cost = myFlight.price2;
+			result.class = "economy";
+			result.Airline = "Hawaiian";
+    		flightsArray[0]=result;
+		}
+    	
+    }
+    console.log(flightsArray);*/
