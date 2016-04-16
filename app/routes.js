029eb1564d8f4c25e62d752d8c5765dff6d71d88
loay <loayelzobeidy@gmail.com>
2016-04-16 19:48:45 +0200

    routes.js middlewear error handled  wt error

--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,5 @@
 module.exports = function(app,mongo) {
-    app.use(function(req, res, next) {
-
-      var token = req.body.wt || req.query.wt || req.headers['x-access-token'];   
-      var jwtSecret = process.env.JWTSECRET;
-      try 
-      {
-        var payload = jwt.verify(token, jwtSecret);
-        req.payload = payload;
-        next();
-      } 
-      catch (err) 
-      {
-        console.error('[ERROR]: JWT Error reason:', err);
-        res.status(403).sendFile(path.join(__dirname, '../public', '403.html'));
-      }
-
-    });
+    
     app.get('/api/data/inflights', function(rep, res){
     	var flights = require('../inFlights.json');
         // console.log(flights);
@@ -46,6 +30,24 @@ module.exports = function(app,mongo) {
     /* RENDER MAIN PAGE */
     app.get('/', function (req, res) {
       res.sendFile(__dirname + '/public/index.html');
+    });
+    app.use(function(req, res, next) {
+       try 
+      {
+      var token = req.body.wt || req.query.wt || req.headers['x-access-token'];   
+      var jwtSecret = process.env.JWTSECRET;
+     
+        var payload = jwt.verify(token, jwtSecret);
+        req.payload = payload;
+        console.log("done");
+        next();
+      } 
+      catch (err) 
+      {
+        console.error('[ERROR]: JWT Error reason:', err);
+        res.status(403).sendFile(path.join(__dirname, '../public', '403.html'));
+      }
+
     });
 
 };
