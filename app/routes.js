25a36dceaeaadf0b597bbb1d46086e6cb86ca51d
RaniaWael <rania.waelsameh.rw@gmail.com>
2016-04-18 00:51:35 +0200

    having fun fixing conflicts

--- app/routes.js
+++ app/routes.js
@@ -131,6 +131,16 @@ module.exports = function(app,mongo) {
         },req.params.origin,req.params.destination,req.params.departingDate,req.params.returningDate,req.params.class);
 
     });
+    
+    app.get('/api/bookings/search/:refNo', function(req, res){
+
+        flights.getBooking(function(err,result){             //new
+
+            res.send(result);
+
+        },req.params.refNo);
+
+    });
 
      app.use(function(req, res, next) {
        try 
