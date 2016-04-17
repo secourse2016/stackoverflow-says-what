239e2ce0bc0ae1e414ec61d4d6f856faa77eb469
Rana Saeed <ranas.2122@gmail.com>
2016-04-17 15:30:45 +0200

    required contactUs view

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -13,8 +13,33 @@ App.controller('mainCtrl', function($scope,flightSrv,$location) {
     $location.url('/bookingDetails');
   };
 
+
+  //contactUs
+  $scope.fn = null;
+  $scope.fnAlert = false;
+  $scope.ln = null;
+  $scope.lnAlert = false;
+  $scope.email = null;
+  $scope.emailAlert = false;
+  $scope.comment = null;
+  $scope.commentAlert = false;
+
   $scope.contactSubmit = function() {
-    $location.url('/thankYou');
+    if($scope.fn == null || $scope.fn == "")
+      $scope.fnAlert = true;
+
+    if($scope.ln == null || $scope.ln == "")
+      $scope.lnAlert = true;
+    
+    if($scope.email == null || $scope.email == "")
+      $scope.emailAlert = true;
+    
+    if($scope.comment == null || $scope.comment == "")
+      $scope.commentAlert = true;
+    
+    else{
+       $location.url('/thankYou');
+    }
   };
 
 });
