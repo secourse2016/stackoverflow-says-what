f7f00f9d8eddf5e84d300bbef2eac62374a0a6e5
loay <loayelzobeidy@gmail.com>
2016-05-02 22:31:34 +0200

    alert message not found done

--- www/js/controllers/SearchCtrl.js
+++ www/js/controllers/SearchCtrl.js
@@ -4,25 +4,42 @@
  $scope.flight= FlightSrv.getFlight();
  if($scope.flight!=undefined)
  console.log($scope.flight.passengers);
- console.log("here");
+ $scope.found = true;
+ // if($scope.searchQuery==FlightSrv.getOldFound())
+ //  $scope.found =false ;
   $scope.searchByRefrence = function() {
        console.log($scope.search.searchQuery);
   	   FlightSrv.setRefNo($scope.search.searchQuery);
        $scope.flight=  FlightSrv.getBookings().success(function(result)
 				{
+ 
 					$scope.flight=result;
 					console.log(result.type);
 					$scope.flight.type = result.type;
+         
                     $state.go("app.bookingInfo");
                     $scope.flight.type=result.type;
 
 					FlightSrv.setFlight($scope.flight);
 					$scope.flight.type = result.type;
                     			console.log($state);
-				});              
- 
-      };
- 
+				
+      }
+        )
+  .error(function(data, status) {
+  console.error('Repos error', status, data);
 
+           $scope.found = false ;
+            // FlightSrv.setOldFound($scope.searchQuery);
+           
+       }
+      ) 
+  .finally(function() {
+  console.log("finally finished repos");
+});   
+   
+
+ 
+};
 
  })
