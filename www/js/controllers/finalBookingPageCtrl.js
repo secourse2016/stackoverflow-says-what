a4db3f3b03e12ceffea7bd6953ccf2c5732a2d8a
Rana Saeed <ranas.2122@gmail.com>
2016-05-02 20:27:49 +0200

    added booking ref num to final view

--- www/js/controllers/finalBookingPageCtrl.js
+++ www/js/controllers/finalBookingPageCtrl.js
@@ -1,11 +1,37 @@
-IonicApp.controller('finalBookingPageCtrl', function($scope, $state, FlightSrv){
+IonicApp.controller('finalBookingPageCtrl', function($scope, $state, FlightSrv,$ionicModal){
 
 $scope.inRefNo = FlightSrv.getInRefNo();
 $scope.outRefNo = FlightSrv.getOutRefNo();
 
-$scope.home = function()
-{
+/*$scope.inRefNo = "batates";
+$scope.outRefNo = "potato";*/
+
+ $scope.showModal = function(animation) {
+    /*console.log(animation);*/
+    $ionicModal.fromTemplateUrl('templates/modal.html', {
+      scope: $scope,
+      animation: 'animated ' + animation,
+      hideDelay:920
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+      $scope.hideModal = function(){
+        $scope.modal.hide();
+        // Note that $scope.$on('destroy') isn't called in new ionic builds where cache is used
+        // It is important to remove the modal to avoid memory leaks
+        $scope.modal.remove();
+      }
+    });
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+
+ $scope.home = function()
+ {
 	$state.go('app.welcomePage')
-};
+ };
 
 });
